bec3ea57412672c08526fcc7c311c264ad7ba849
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-08-28 16:47:10 -0500

    commit
    text

--- app/src/main/java/com/java/movilasofar/vista/barcode.java
+++ app/src/main/java/com/java/movilasofar/vista/barcode.java
@@ -19,23 +19,19 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.java.movilasofar.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link barcode#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class barcode extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
     public Button btnScanner;
     public TextView tvBarCode;
+    public EditText codigoProLabel;
     public Context c;
 
 
@@ -64,10 +60,14 @@ public class barcode extends Fragment {
         btnScanner = root.findViewById(R.id.btnScanner);
         tvBarCode = root.findViewById(R.id.tvBarCode);
 
+
+
         btnScanner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                codigoProLabel = (EditText) getActivity().findViewById(R.id.codigoProlabel);
                 escanear();
+                codigoProLabel.setText(tvBarCode.getText().toString());
             }
         });
         c = root.getContext();
@@ -76,7 +76,7 @@ public class barcode extends Fragment {
         return root;
     }
 
-
+//escaneo de codigo de barras-----------------
     public void escanear(){
         IntentIntegrator intent = IntentIntegrator.forSupportFragment(barcode.this);
         intent.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
@@ -86,7 +86,7 @@ public class barcode extends Fragment {
         intent.setBarcodeImageEnabled(false);
         intent.initiateScan();
     }
-
+//------------------------------------------------------------------
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
@@ -103,28 +103,7 @@ public class barcode extends Fragment {
     }
 
 
-    /*  @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        IntentResult result= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-            if(result != null)
-                if (result.getContents() != null){
-                    tvBarCode.setText(result.getContents());
-                }else{
-                    tvBarCode.setText("Error al escanear el codigo de barras");
-                }
-    }*/
-/*
-    public View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.btnScanner:
-                    new IntentIntegrator(getActivity()).initiateScan();
-                    break;
-            }
-        }
-    };*/
+
 
 
 
